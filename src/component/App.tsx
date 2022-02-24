6a5217ebcecd22f6351466f913e4052911547fa9
Radoslav Velichkov <rado88x@gmail.com>
2022-02-24 17:05:13 +0200

    added CSS plugins and styles for App and Pizza cmp

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import pizzas from "../data/pizzas.json";
+import Pizza from "./Pizza";
+
+import AppCSS from "./App.module.css";
 
 const App = () => {
-
-  return <div>SomethingS</div>;
+  return (
+    <div className={AppCSS.container}>
+    <ul>
+      {pizzas.map(pizza=> {
+        return <Pizza key={pizza.id} pizza={pizza}/>
+      })}
+    </ul>
+    </div>
+  )
 };
 
 export default App;
-
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/App";
 
+import "../styles/main.css";
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
