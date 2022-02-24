f3201fa7bffa07c92963f42862e8ac50642c8f28
Radoslav Velichkov <rado88x@gmail.com>
2022-02-24 16:02:08 +0200

    added App cmp

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: './src/index.tsx',
+  devtool: "eval-source-map",
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
+      template: './src/index.html',
+      filename: 'index.html',
     }),
   ],
-//   stats: {
-//     children: true,
-//   },
 };
