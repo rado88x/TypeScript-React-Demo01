7d79bca6812c0e45eb0675a37120c342fad6bbff
Radoslav Velichkov <rado88x@gmail.com>
2022-02-25 18:48:38 +0200

    added special offer component

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -6,24 +6,30 @@ import Pizza from "./Pizza";
 import AppCSS from "./App.module.css";
 import PizzaPictureCmp from "../component/PizzaPictureCmp";
 import Cart from "./Cart";
+import AppStateProvider from "./AppState";
+import SpecialOffer from "./SpecialOffer";
 
 const App = () => {
+  const specialofferPizza = pizzas.find((pizzas) => pizzas.specialOffer);
+
   return (
-    <div className={AppCSS.container}>
-      <div className={AppCSS.header}>
-        <div>
-        
+    <AppStateProvider>
+      <div className={AppCSS.container}>
+        <div className={AppCSS.header}>
+          <div></div>
+          <PizzaPictureCmp />
+          <Cart>
+            <div className={AppCSS.siteTitle}>Delicious Italian Pizza</div>
+          </Cart>
         </div>
-        <PizzaPictureCmp />
-        <Cart></Cart>
-        <div className={AppCSS.siteTitle}>Delicious Italian Pizza</div>
+        {specialofferPizza && <SpecialOffer pizza={specialofferPizza} />}
+        <ul className={AppCSS.pizzaList}>
+          {pizzas.map((pizza) => {
+            return <Pizza key={pizza.id} pizza={pizza} />;
+          })}
+        </ul>
       </div>
-      <ul>
-        {pizzas.map((pizza) => {
-          return <Pizza key={pizza.id} pizza={pizza} />;
-        })}
-      </ul>
-    </div>
+    </AppStateProvider>
   );
 };
 
--- src/component/Cart.tsx
+++ src/component/Cart.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { FiShoppingCart } from "react-icons/fi";
-import CartCSS from "./Cart.module.css";
+import React from 'react';
+import { FiShoppingCart } from 'react-icons/fi';
+import CartCSS from './Cart.module.css';
+import { AppStateContext } from './AppState';
 
 interface Props {}
 
 interface State {
-    isOpen : boolean;
+  isOpen: boolean;
 }
 
 class Cart extends React.Component<Props, State> {
@@ -14,36 +15,52 @@ class Cart extends React.Component<Props, State> {
     this.state = {
       isOpen: false,
     };
-    // this.handlerClick = this.handlerClick.bind();
   }
 
-    handlerClick = (e : React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    // if ((e.target as HTMLElement).nodeName === 'SPAN') {
+    //   (e.target as HTMLSpanElement).;
+    // }
     this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
-  }
+  };
 
   render() {
     return (
-      <div className={CartCSS.cartContainer}>
-        <button
-          className={CartCSS.button}
-          type="button"
-          onClick={this.handlerClick}
-        >
-          <FiShoppingCart />
-          <span>2 pizza(s)</span>
-        </button>
-        <div
-          className={CartCSS.cartDropDown}
-          style={{
-            display: this.state.isOpen ? 'block' : 'none',
-          }}
-        >
-          <ul>
-            <li>Napoletana</li>
-            <li>Marinara</li>
-          </ul>
-        </div>
-      </div>
+      <AppStateContext.Consumer>
+        {(state) => {
+          const itemsCount = state.cart.items.reduce((sum, item) => {
+            return sum + item.quantity;
+          }, 0);
+          return (
+            <div className={CartCSS.cartContainer}>
+              <button
+                className={CartCSS.button}
+                type="button"
+                onClick={this.handleClick}
+              >
+                <FiShoppingCart />
+                <span>{itemsCount} pizza(s)</span>
+              </button>
+              <div
+                className={CartCSS.cartDropDown}
+                style={{
+                  display: this.state.isOpen ? 'block' : 'none',
+                }}
+              >
+                <ul>
+                  {state.cart.items.map((item) => {
+                    return (
+                      <li key={item.id}>
+                        {item.name} &times; {item.quantity}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          );
+        }}
+      </AppStateContext.Consumer>
     );
   }
 }
